5abdd478926e424cf421f640f1750cd7d14fef56
Karim Yarboua <karim.yarboua@open-groupe.com>
2019-03-07 11:30:57 +0100

    Initial Commit

---
+++ Calanques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Calanques
+//
+//  Created by Karim Yarboua on 07/03/2019.
+//  Copyright © 2019 Karim Yarboua. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
